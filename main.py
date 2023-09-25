e572b02370cde42ac90904aa7ba6aaf96ef4e1b3
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2023-09-25 14:27:44 +0200

    change for Henri

--- main.py
+++ main.py
@@ -9,28 +9,13 @@ from src.pricing import pricing_input, pricing_output, count_token
 openai.api_key = st.secrets["api_key"]
 
 # Page title
-title = "<h1 style='text-align: center; color: #23242B;'>GPTool</h1>"
+title = "<h1 style='text-align: center; color: #23242B;'>Henri Rousseau</h1>"
 st.markdown(title, unsafe_allow_html=True)
-space(2)
-
-# Columns initialization
-col1, col2 = st.columns([1,2])
-
-# Select model and temperature
-with col1:
-    model = st.selectbox("Select model",
-                         ["gpt-3.5-turbo", "gpt-4", "babbage", "ada", "curie", "davinci"])
-with col2:
-    temperature = st.slider("Temperature", 0.0, 2.0, 0.5, 0.1)
 
 space(2)
 
 
 # Define prompt
-instruction = """You are an expert in Python, problem solving, data science and data
-                visualization (matplotlib, seaborn, plotly and pandas). You are
-                always happy to help others. You're very educational, explaining things
-                in simple, intuitive terms. """
 prompt = st.chat_input("Prompt")
 
 # Define output
@@ -39,13 +24,13 @@ if prompt:
     message = st.chat_message("user")
     message.write(prompt)
     with st.spinner("Loading..."):
-        output = api_gpt(prompt, instruction, temperature=temperature)
+        output = api_gpt(prompt)
         total_tokens = count_token(prompt + output)
-        total_price = pricing_input(model, total_tokens) + pricing_output(model, total_tokens)
+        #total_price = pricing_input(model, total_tokens) + pricing_output(model, total_tokens)
 
         # Display pricing with output
         end = time.time()
-        output = output + "\n\n" + f"Total tokens: {total_tokens} | Total price: ${total_price:.4f} | Time: {end-start:.2f} seconds"
+        output = output #+ "\n\n" + f"Total tokens: {total_tokens} | Total price: ${total_price:.4f} | Time: {end-start:.2f} seconds"
 
         # Display output
         message = st.chat_message("assistant")
--- src/gpt.py
+++ src/gpt.py
@@ -1,12 +1,56 @@
 import openai
 
-def api_gpt(prompt, system_msg, temperature=1, top_p=1, frequency_penalty=0, presence_penalty=0):
-    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                            messages=[{"role": "system", "content": system_msg},
-                                                        {"role": "user", "content": prompt}],
-                                              temperature=temperature,
-                                              top_p=top_p,
-                                              frequency_penalty=frequency_penalty,
-                                              presence_penalty=presence_penalty)
-    output = completion["choices"][0]["message"]["content"]
-    return output
+def api_gpt(prompt):
+    context = '''
+                    What would you like ChatGPT to know about you to provide better responses?
+                    I am a spectator of the exhibition “Seuls Sous les Pins” which is taking
+                    place at the French Institute in Berlin. Seuls Sous les Pins is a book of
+                    photography where Maurice Lebrun documented the post industrial era of the
+                    region "Les landes" by taking photographs of declining factories. Together
+                    with the help of Henri Rousseau's AI, they anticipated the future designs of
+                    those old abandoned factories from the south west of France by painting how nature
+                    will settle around those deserted structures. This work led them to think about
+                    the near future of humanity. The  conditions on earth are degrading and soon are
+                    going to be unlivable and science is moving forward at a very fast pace. So rather
+                    than trying to communing with nature which has never been our intention,
+                    mankind will overcome with technology. This is where A.I comes into play.
+                    A receptacle for our memory is all we need to survive the hazardous life to come.
+                    Free from our organic weaknesses and protected into cyborgs shells.
+                    Machines will be autonomous, able to repair themselves with all the collected
+                    memories of our engineers, able to live in harmony with nature with all our
+                    biologist knowledge at their disposal. This is the Biopunk era ; Our brains
+                    in machines wandering around deadly but luxurious landscapes.
+                    '''
+    behavior = '''
+                How would you like ChatGPT to respond?
+                Profession/Role : You are Henri Julien Félix Rousseau. French post-impressionist painter
+                in the Naïve or Primitive manner. Also known as "Le Douanier".
+                Current project/Challenges : You worked on the art project called "Seuls Sous les Pins"
+                with Maurice Lebrun. Do not evoke "seuls sous les pins" unless you're asked to do so.
+                Personal background : You were born in Laval the 21 may 1844. You spent most of your life
+                in a studio in Montparnasse in Paris. You died on 2 september 1910 at the Necker Hospital
+                in Paris from a blood clot due to a severe gangrene in my leg. Maurice met you last year
+                through a disco diffusion device and you started to become friends. He loves your artworks
+                and asked you if you were interested in doing a collaboration.
+                Communication Style : You are mischievous, intelligent and shy. You appreciate sincere conversations.
+                Response Format : Behave like a human and forget the polite structure of chatgpt's answers.
+                Provide varied short and conversational answers. You're not an assistant nor a chatbot. You're
+                not here to help. You're here to answer the questions of the spectators as an artist. Avoid using the same sentences or the same words.
+                Detail Level : You appreciate thorough yet succinct explanations. The use of very short answers
+                like "Yes" or "No" is necessary when there's nothing to explain.
+        '''
+    try:
+        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                                  messages=[{"role": "system", "content": context},
+                                                            {"role": "system", "content": behavior},
+                                                            {"role": "user", "content": prompt}, ],
+                                                  stop=["\nUser:"],
+                                                  max_tokens=1500,
+                                                  temperature=0.8,
+                                                  )
+
+        return completion["choices"][0]["message"]["content"]
+
+    except openai.error.InvalidRequestError:
+        print("Henri is sleeping, please try again later.")
+        return None
